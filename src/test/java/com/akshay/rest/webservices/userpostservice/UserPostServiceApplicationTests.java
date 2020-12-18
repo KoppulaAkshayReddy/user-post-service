74c61c38e2badd2f1fd9c12f5b588619f0885841
Koppula Akshay Reddy <akshay@koppulas-air.home>
2020-12-18 13:54:30 -0600

    initial rest service commit

---
+++ src/test/java/com/akshay/rest/webservices/userpostservice/UserPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.rest.webservices.userpostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
